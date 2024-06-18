07573ed44e8d974ae4e9577651fcfda9e970bf46
suraj <surajcoding243@gmail.com>
2024-06-19 02:02:25 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter'});
+const ibmplexserif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400','700'],
+  variable: '--font-ibm-plex-serif'
+})
+export const metadata: Metadata = {
+  title: "Vertical",
+  description: "climb the ladder of banking",
+  icons: {
+       icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable}${ibmplexserif.variable}`}>{children}</body>
+    </html>
+  );
+}
